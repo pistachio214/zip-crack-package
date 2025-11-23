0a5a2fae948ff4c745b6d6101e059207985e49cf
pengsy <songyang410@outlook.com>
2025-11-23 22:53:19 +0800

    opt: rar破解类初始实现

--- src/rar_module/handle.rs
+++ src/rar_module/handle.rs
@@ -1,4 +1,12 @@
+use unrar::Archive;
+
 /// TODO rar格式解压
-pub fn try_extract_rar(_input_path: &str) {
-    // something...
+pub fn try_extract_rar(input_path: &str) {
+
+}
+
+fn is_password_required(file_path: &str) -> bool {
+    let archive = Archive::new(file_path);
+
+    false
 }
